2641885f47abe95d0fe50cd670ddf21418a6e1a3
Taylor <tfix001@ucr.edu>
2020-03-31 15:47:43 -0700

    Checking if it uploads properly

--- Lab1/src/Lab1Main.java
+++ Lab1/src/Lab1Main.java
@@ -1,5 +1,8 @@
 public class Lab1Main {
     public static void main(String[] args){
         System.out.println("Hello World");
+        /*
+        * Let's see this in the Github File please!
+        */
     }
 }
